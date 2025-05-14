36d3f8be7b8195e00f40f4205e07083dcf8bdde7
niconett18 <nicholasedmund18@gmai.com>
2025-05-15 00:04:32 +0700

    Added documentation to add function

--- calculator.c
+++ calculator.c
@@ -2,6 +2,13 @@
 float subtract(float a, float b) {
     // TODO: implement subtraction
 }
+/**
+ * Adds two floating-point numbers.
+ *
+ * @param a The first number.
+ * @param b The second number.
+ * @return The sum of a and b.
+ */
 float add(float a, float b) {
     return a + b;
 }
