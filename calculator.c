43d36b94218c06a8a358533f1cf82ea8bee1d35d
Tubagus Dafa <tubagusdafa@Dafas-MacBook-Air.local>
2025-05-15 00:07:33 +0700

    Added documentation to multiply function

--- calculator.c
+++ calculator.c
@@ -5,6 +5,13 @@ float subtract(float a, float b) {
 float add(float a, float b) {
     // TODO: implement addition
 }
+/**
+ * Multiplies two floating-point numbers.
+ *
+ * @param a The first number.
+ * @param b The second number.
+ * @return The product of a and b.
+ */
 float multiply(float a, float b) {
     return a * b;
 }
