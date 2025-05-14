5376830c5f0aabbde6aac448b781a58a7a2012d6
rifqifadill <rifqiitsnain7@gmail.com>
2025-05-15 00:06:13 +0700

    Added documentation to subtract function

--- calculator.c
+++ calculator.c
@@ -1,6 +1,14 @@
+/**
+ * Subtracts the second floating-point number from the first.
+ *
+ * @param a The number to subtract from.
+ * @param b The number to subtract.
+ * @return The difference between a and b.
+ */
 
 float subtract(float a, float b) {
-    // TODO: implement subtraction
+    return a-b;
+
 }
 float add(float a, float b) {
     // TODO: implement addition
